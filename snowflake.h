596f322a68764b9bf7b44e0294d324db0c685344
John Cook <watfordjc@users.noreply.github.com>
2021-09-03 01:11:57 +0000

    Change SNOWFLAKE_EPOCH to midnight 2021

--- snowflake.h
+++ snowflake.h
@@ -24,7 +24,7 @@ THE SOFTWARE.
 #define __SNOWFLAKE__
 
 // the timestamp in milliseconds of the start of the custom epoch
-#define SNOWFLAKE_EPOCH 1388534400000 //Midnight January 1, 2014
+#define SNOWFLAKE_EPOCH 1609459200000 // Midnight UTC, January 1, 2021
 
 #define SNOWFLAKE_TIME_BITS 41
 #define SNOWFLAKE_REGIONID_BITS 4
